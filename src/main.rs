f8a0e5a4e43c688104ef093803e5eb7a7609f0b3
Dliteofficial <opeyemi4adewole@gmail.com>
2024-10-19 18:58:38 +0100

    Basic To-Do App in Rust Done

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,8 @@
-//Create To-do
-//Update todo with new items
-//delete todo items from list
-//read todo item
-
-//Items: vector of items
-//each item:
-//Date & Time todo was made
-//title of item
-//description/details
-
 use chrono::{DateTime, Local};
 
+#[derive(Debug)]
 struct Item {
-    date_and_time: String,
+    date_and_time: DateTime<Local>,
     item_title: String,
     item_description: String
 }
@@ -20,13 +10,26 @@ struct Item {
 impl Item {
     fn new(title: String, description: String) -> Self{
         Item { 
-            date_and_time: Local::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+            date_and_time: Local::now(),
             item_title: title,
             item_description: description
         }
     }
+
+    fn fetch_title (&self) -> &str {
+        &self.item_title
+    }
+
+    fn fetch_description (&self) -> &str {
+        &self.item_description
+    }
+
+    fn fetch_date (&self) -> String {
+        self.date_and_time.format("%Y-%m-%d %H:%M").to_string()
+    }
 }
 
+#[derive(Debug)]
 struct ToDo {
     items: Vec<Item>
 }
@@ -36,19 +39,36 @@ impl ToDo {
         ToDo { items: vec![] }
     }
 
-    fn addItem(&mut self, item: Item) {
+    fn add_item(&mut self, item: Item) {
         self.items.push(item);
     }
 
-    fn removeItem(&mut self, index: usize) {
-        self.items.remove(index);
+    fn remove_item(&mut self, index: usize) -> Option<Item>{
+        if self.items.len() > index {
+            Some(self.items.remove(index))
+        }else {
+            None
+        }
     }
 
-    fn fetch_Item(&self, index: usize) -> &Item {
-        &self.items[index]
+    fn fetch_item(&self, index: usize) -> Option<&Item> {
+        self.items.get(index)
     }
 }
 
 fn main() {
 
+    let new_item = Item::new(String::from("SomeTitle"), String::from("SomeDescription"));
+
+    let mut todo_list = ToDo::new();
+    todo_list.add_item(new_item);
+
+    match todo_list.fetch_item(0) {
+        Some(value) => {
+            println!("Title: {:#?}", value.fetch_title());
+            println!("Date: {:#?}", value.fetch_date());
+            println!("Description: {:#?}", value.fetch_description());
+        },
+        None => println!("Index Out of Bounds")
+    }
 }
