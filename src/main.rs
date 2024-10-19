17f60bab899d7b87c8d8ab3e9ee98f4cb5c1eaa5
Dliteofficial <opeyemi4adewole@gmail.com>
2024-10-19 20:10:19 +0100

    Aded CLI capabilities to App

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use chrono::{DateTime, Local};
+use std::io::{self, Write};
 
 #[derive(Debug)]
 struct Item {
@@ -54,21 +55,63 @@ impl ToDo {
     fn fetch_item(&self, index: usize) -> Option<&Item> {
         self.items.get(index)
     }
+
+    fn list_items(&self) {
+        self.items.iter().for_each(|item|println!("{} - {}", item.fetch_date(), item.fetch_title()));
+    }
+}
+
+//function to read input from the user
+fn read_input(prompt: &str) -> String {
+    print!("{}", prompt);
+    io::stdout().flush().unwrap(); //ensures that the prompt is shown
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("failed to read input");
+    input.trim().to_string()
 }
 
 fn main() {
-
-    let new_item = Item::new(String::from("SomeTitle"), String::from("SomeDescription"));
-
     let mut todo_list = ToDo::new();
-    todo_list.add_item(new_item);
 
-    match todo_list.fetch_item(0) {
-        Some(value) => {
-            println!("Title: {:#?}", value.fetch_title());
-            println!("Date: {:#?}", value.fetch_date());
-            println!("Description: {:#?}", value.fetch_description());
-        },
-        None => println!("Index Out of Bounds")
+    loop {
+        println!("\n-- To-Do App --\n");
+        println!("1. Add Item");
+        println!("2. Remove Item");
+        println!("3. Fetch Item");
+        println!("4. List Item");
+        println!("5. Quit");
+
+        let choice = read_input("Choose and Option: ");
+        match choice.as_str() {
+            "1" => {
+                let title = read_input("Enter item title: ");
+                let description = read_input("Enter item description: ");
+                let new_item = Item::new(title, description);
+                todo_list.add_item(new_item);
+                println!("Item added!");
+            },
+            "2" => {
+                let index = read_input("Enter the index of the item: ").parse().expect("Please enter a valid number");
+                match todo_list.remove_item(index) {
+                    Some(_) => println!("Item Removed"),
+                    None => println!("Invalid Index")
+                }
+            },
+            "3" =>{
+                let index = read_input("Enter the index of the item: ").parse().expect("Please enter a valid number");
+                match todo_list.fetch_item(index) {
+                    Some(value) => println!("{} was created on {} and has the following as description: {}", value.item_title, value.fetch_date(), value.item_description),
+                    None => println!("Invalid Index")
+                }
+            },
+            "4" => {
+                todo_list.list_items();
+            },
+            "5" => {
+                println!("Goodbye!");
+                break;
+            },
+            _ => println!("Invalid choice. Please try again."),
+        }
     }
 }
